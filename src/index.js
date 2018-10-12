8ea05feca9ed22d6edf1c6d7282b779670dfcbc4
xhf394 <xhf394@163.com>
2018-10-13 00:28:21 +0800

    start this project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class WeatherApp extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+            <div className="header">
+            <form>
+            <input type="text" placeholder="Search City" />
+            </form>
+            
+            </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<WeatherApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
