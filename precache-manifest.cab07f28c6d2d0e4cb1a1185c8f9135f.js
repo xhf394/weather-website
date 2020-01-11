bf111e05b14dcace74f18ab9655c61ac86bee045
xhf394 <39177879+xhf394@users.noreply.github.com>
2020-01-11 23:29:51 +0800

    Updates

---
+++ precache-manifest.cab07f28c6d2d0e4cb1a1185c8f9135f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcebca8ec033e125bdb4",
+    "url": "/weather-website/static/js/runtime~main.dcebca8e.js"
+  },
+  {
+    "revision": "4c185d146b692556c9a3",
+    "url": "/weather-website/static/js/main.4c185d14.chunk.js"
+  },
+  {
+    "revision": "d5a2b48ca6790a9fbf75",
+    "url": "/weather-website/static/js/2.d5a2b48c.chunk.js"
+  },
+  {
+    "revision": "4c185d146b692556c9a3",
+    "url": "/weather-website/static/css/main.8d898598.chunk.css"
+  },
+  {
+    "revision": "53b4fea73b6f7a63283602cdfa4383bf",
+    "url": "/weather-website/index.html"
+  }
+];
